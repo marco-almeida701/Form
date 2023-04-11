178c8d94a3aaea34255560a0c5bce818cac9fa48
Marco Almeida <marco.almeida701@hotmail.com>
2023-04-11 20:56:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+/* incompleto
+function concluido() {
+        let nome = document.getElementById('name')
+        let subn = document.getElementById('subn')
+        let mail = document.getElementById('mail')
+        let res = document.getElementById('res')
+
+    if (nome.value.length == 0 || subn.value.length == 0 || mail.value.length == 0) {
+        window.alert('Preencha o formulário corretamente')
+    }   
+}
+*/
