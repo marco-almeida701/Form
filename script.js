14ba223f9641e0571a80d8862d2a45a3d1e6de2e
Marco Almeida <marco.almeida701@hotmail.com>
2023-04-12 23:45:09 -0300

    Update Script.js

--- script.js
+++ script.js
@@ -5,27 +5,28 @@ form.addEventListener('submit', function(event) {
   event.preventDefault();
 
   const nome = document.querySelector('#Name').value
-  const subn = document.querySelector('#subn').value
-  const mai = document.querySelector('#mail').value
-  if (nome.trim() === '' || subn.trim() === '' || mai.trim() === '') {
+  if (nome.trim() === '') {
     alert('Preencha os campos corretamente')
-    return;
-  }
+    return
+  } 
 
-  const sobrenome = document.querySelector('#subn').value
+  const subn = document.querySelector('#subn').value
   const email = document.querySelector('#mail').value
   const lado = document.querySelector('input[name="devweb"]:checked').id
-  const senioridade = document.querySelector('#Senioridade').options[document.querySelector('#Senioridade').selectedIndex].value;
+  const select = document.querySelector('#Senioridade').options[document.querySelector('#Senioridade').selectedIndex].value;
   const tecnologias = [];
   document.querySelectorAll('input[type="checkbox"]:checked').forEach(function(checkbox) {
     tecnologias.push(checkbox.nextElementSibling.textContent)
-  });
+  })
   const experiencia = document.querySelector('#exp').value
 
-  resultado.innerHTML = `<strong>Nome: </strong> ${nome} ${sobrenome}<br>
+  if(subn.trim() === '' || email.trim() === ''){
+  alert('Preencha os campos corretamente')
+}
+  resultado.innerHTML = `<strong>Nome: </strong> ${nome} ${subn}<br>
                             <strong>E-mail: </strong> ${email}<br>
                             <strong>Lado da aplicação: </strong> ${lado}<br>
-                            <strong>Senioridade: </strong>${senioridade}<br>
+                            <strong>Senioridade: </strong>${select}<br>
                             <strong>Tecnologias: </strong>${tecnologias.join(', ')}<br>
                             <strong>Experiência: </strong>${experiencia}`
-});
+})
