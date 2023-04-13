62cf2ef225dee892161715fa483b6fc904c667f7
marco-almeida701 <marco.almeida701@hotmail.com>
2023-04-12 22:48:26 -0300

    Merge branch 'main' of https://github.com/marco-almeida701/Formulario

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-/* incompleto
-function concluido() {
-        let nome = document.getElementById('name')
-        let subn = document.getElementById('subn')
-        let mail = document.getElementById('mail')
-        let res = document.getElementById('res')
+const form = document.querySelector('form');
+const resultado = document.querySelector('#res');
 
-    if (nome.value.length == 0 || subn.value.length == 0 || mail.value.length == 0) {
-        window.alert('Preencha o formulário corretamente')
-    }   
-}
-*/
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const nome = document.querySelector('#Name').value;
+  if (nome.trim() === '') {
+    resultado.textContent = 'Preencha o campo Nome';
+    return;
+  }
+
+  const sobrenome = document.querySelector('#subn').value;
+  const email = document.querySelector('#mail').value;
+  const lado = document.querySelector('input[name="devweb"]:checked').id;
+  const senioridade = document.querySelector('#Senioridade').options[document.querySelector('#Senioridade').selectedIndex].value;
+  const tecnologias = [];
+  document.querySelectorAll('input[type="checkbox"]:checked').forEach(function(checkbox) {
+    tecnologias.push(checkbox.nextElementSibling.textContent);
+  });
+  const experiencia = document.querySelector('#exp').value;
+
+  resultado.innerHTML = `Nome: ${nome} ${sobrenome}<br>
+                          E-mail: ${email}<br>
+                          Lado da aplicação: ${lado}<br>
+                          Senioridade: ${senioridade}<br>
+                          Tecnologias: ${tecnologias.join(', ')}<br>
+                          Experiência: ${experiencia}`;
+});
