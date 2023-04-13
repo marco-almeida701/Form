01367bdcc1dd9507e8ee98be7d186de2d699fa8e
Marco Almeida <marco.almeida701@hotmail.com>
2023-04-12 23:24:20 -0300

    update Scritp.js

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-const form = document.querySelector('form');
-const resultado = document.querySelector('#res');
+const form = document.querySelector('form')
+const resultado = document.querySelector('#res')
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
 
-  const nome = document.querySelector('#Name').value;
-  if (nome.trim() === '') {
-    resultado.textContent = 'Preencha o campo Nome';
+  const nome = document.querySelector('#Name').value
+  const subn = document.querySelector('#subn').value
+  const mai = document.querySelector('#mail').value
+  if (nome.trim() === '' || subn.trim() === '' || mai.trim() === '') {
+    alert('Preencha os campos corretamente')
     return;
   }
 
-  const sobrenome = document.querySelector('#subn').value;
-  const email = document.querySelector('#mail').value;
-  const lado = document.querySelector('input[name="devweb"]:checked').id;
+  const sobrenome = document.querySelector('#subn').value
+  const email = document.querySelector('#mail').value
+  const lado = document.querySelector('input[name="devweb"]:checked').id
   const senioridade = document.querySelector('#Senioridade').options[document.querySelector('#Senioridade').selectedIndex].value;
   const tecnologias = [];
   document.querySelectorAll('input[type="checkbox"]:checked').forEach(function(checkbox) {
-    tecnologias.push(checkbox.nextElementSibling.textContent);
+    tecnologias.push(checkbox.nextElementSibling.textContent)
   });
-  const experiencia = document.querySelector('#exp').value;
+  const experiencia = document.querySelector('#exp').value
 
   resultado.innerHTML = `<strong>Nome: </strong> ${nome} ${sobrenome}<br>
                             <strong>E-mail: </strong> ${email}<br>
                             <strong>Lado da aplicação: </strong> ${lado}<br>
                             <strong>Senioridade: </strong>${senioridade}<br>
                             <strong>Tecnologias: </strong>${tecnologias.join(', ')}<br>
-                            <strong>Experiência: </strong>${experiencia}`;
+                            <strong>Experiência: </strong>${experiencia}`
 });
